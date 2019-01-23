f7321c01165f83159167dbcc4fa036e6529d52b3
Cassondria19 <46826356+Cassondria19@users.noreply.github.com>
2019-01-22 21:34:04 -0700

    Add files via upload

---
+++ MyFirstProject/src/Introduction.java
@@ -0,0 +1,12 @@
+//Cassondiria White
+// Date:1/22/2019
+public class Introduction {
+
+	public static void main(String[] args) {
+		// print name favorite color and if won the lottery on console
+    System.out.println("My Name is Cassondria White.");
+    System.out.println("My favorite color is blue.");
+    System.out.println("If I won the lottery I would help the homeless.");
+	}
+
+}
